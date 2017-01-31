d92490a176f2288691df5a455bb5e8996795b699
Marcos Moura <marcosvmmoura@gmail.com>
2017-01-31 06:09:59 -0200

    remove unused dependency

--- src/app/config.js
+++ src/app/config.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
 import Axios from 'axios'
-import mitt from 'mitt'
 
 
 /* HTTP Configs */
 Vue.prototype.$http = Axios
 Axios.defaults.baseURL = process.env.API_URL
 Axios.defaults.headers.common['Content-Type'] = 'application/json';
-
-
-/* Event Handler */
-Vue.prototype.$events = mitt()
